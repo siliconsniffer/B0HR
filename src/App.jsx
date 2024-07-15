2abae336ead36fe57bc9b706205c2d19a1cb37ff
siliconsniffer <uneakxx8rq@posteo.mx>
2024-07-15 12:08:00 +0200

    math is doing funny things

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Trail } from "@react-three/drei";
 import { useRef, useState } from "react";
 import "./App.css";
 
@@ -10,15 +10,24 @@ const Sphere = ({ position, size, color }) => {
   useFrame((state, delta) => {
     ref.current.rotation.z += delta;
     ref.current.rotation.y += 0.5 * delta;
-    ref.current.position.x = Math.sin(state.clock.elapsedTime) * Math.PI * 2;
-    ref.current.position.z = Math.cos(state.clock.elapsedTime) * Math.PI * 2;
+    ref.current.position.x = Math.sin(state.clock.elapsedTime) * Math.PI * 0.75;
+    ref.current.position.z =
+      Math.cos(state.clock.elapsedTime * 0.5) * Math.PI * 0.75;
+    ref.current.position.y =
+      Math.cos(state.clock.elapsedTime * 0.5) * Math.PI * 0.75;
     // console.log(state.clock.elapsedTime);
   });
   return (
-    <mesh position={position} ref={ref} onClick={(event) => setActive(!active)}>
-      <sphereGeometry args={size} />
-      <meshStandardMaterial color={active ? "hotpink" : color} />
-    </mesh>
+    <Trail width={0.2} color={"gray"} length={67} decay={1} local={false}>
+      <mesh
+        position={position}
+        ref={ref}
+        onClick={(event) => setActive(!active)}
+      >
+        <sphereGeometry args={size} />
+        <meshStandardMaterial color={active ? "hotpink" : color} wireframe />
+      </mesh>
+    </Trail>
   );
 };
 
@@ -28,12 +37,12 @@ function App() {
       <OrbitControls maxDistance={25} minDistance={1} makeDefault />
       <directionalLight intensity={0.75} position={(0, 0, 2)} />
       <ambientLight intensity={0.25} />
-      <axesHelper args={[5]} />
+      {/* <axesHelper args={[5]} /> */}
 
       <Sphere position={[-4, 2, -4]} color={"ForestGreen"} />
-      <Sphere position={[-1, -2, 0]} color={"DarkOrange"} />
+      {/* <Sphere position={[-1, -2, 0]} color={"DarkOrange"} />
       <Sphere color={"Gold"} />
-      <Sphere color={"SandyBrown"} />
+      <Sphere color={"SandyBrown"} /> */}
     </Canvas>
   );
 }
